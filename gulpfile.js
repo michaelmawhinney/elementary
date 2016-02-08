fa09521dbc2b8814d1da4e48b71b3f0a075c448c
Michael R Mawhinney Jr <mike@mawhinney.xyz>
2016-02-08 03:06:49 -0500

    set csscomb options, cleaned up the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var gulp          = require('gulp');
 var runsequence   = require('run-sequence');
 var del           = require('del');
 var rename        = require('gulp-rename');
-//var plumber       = require('gulp-plumber');
 
 // css
 var sass          = require('gulp-sass');
@@ -17,12 +16,35 @@ var cssmin        = require('gulp-cssmin');
 
 gulp.task('sass', function(){
   return gulp.src('scss/elementary.scss')
-//    .pipe(plumber())
     .pipe(sass({precision:8}))
     .pipe(autoprefixer({
       browsers: ['last 5 firefox versions','last 5 chrome versions']
     }))
-    .pipe(csscomb())
+    .pipe(csscomb({
+    "remove-empty-rulesets": true,
+    "always-semicolon": true,
+    "color-case": "lower",
+    "block-indent": "  ",
+    "color-shorthand": true,
+    "element-case": "lower",
+    "eof-newline": false,
+    "leading-zero": false,
+    "sort-order-fallback": "abc",
+    "space-before-colon": "",
+    "space-after-colon": " ",
+    "space-before-combinator": " ",
+    "space-after-combinator": " ",
+    "space-between-declarations": "\n",
+    "space-before-opening-brace": " ",
+    "space-after-opening-brace": "\n",
+    "space-after-selector-delimiter": "\n",
+    "space-before-selector-delimiter": "",
+    "space-before-closing-brace": "\n",
+    "strip-spaces": true,
+    "tab-size": true,
+    "unitless-zero": true,
+    "vendor-prefix-align": true
+}))
     .pipe(gulp.dest('dist'))
 });
 
