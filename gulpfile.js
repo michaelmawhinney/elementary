3afa37970507b9edd3ea10c4efca58bc8f827bc1
Michael R Mawhinney Jr <mike@mawhinney.xyz>
2016-02-08 12:45:23 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,33 +18,9 @@ gulp.task('sass', function(){
   return gulp.src('scss/elementary.scss')
     .pipe(sass({precision:8}))
     .pipe(autoprefixer({
-      browsers: ['last 5 firefox versions','last 5 chrome versions']
+      browsers: ['last 2 firefox versions','last 2 chrome versions']
     }))
-    .pipe(csscomb({
-    "remove-empty-rulesets": true,
-    "always-semicolon": true,
-    "color-case": "lower",
-    "block-indent": "  ",
-    "color-shorthand": true,
-    "element-case": "lower",
-    "eof-newline": false,
-    "leading-zero": false,
-    "sort-order-fallback": "abc",
-    "space-before-colon": "",
-    "space-after-colon": " ",
-    "space-before-combinator": " ",
-    "space-after-combinator": " ",
-    "space-between-declarations": "\n",
-    "space-before-opening-brace": " ",
-    "space-after-opening-brace": "\n",
-    "space-after-selector-delimiter": "\n",
-    "space-before-selector-delimiter": "",
-    "space-before-closing-brace": "\n",
-    "strip-spaces": true,
-    "tab-size": true,
-    "unitless-zero": true,
-    "vendor-prefix-align": true
-}))
+    .pipe(csscomb())
     .pipe(gulp.dest('dist'))
 });
 
